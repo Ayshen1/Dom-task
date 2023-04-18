90b49eedb029886639f54594268e8ce64c0ec9d8
Ayshen1 <aysnzizova1231@gmail.com>
2023-04-18 04:23:03 +0400

    added

---
+++ script.js
@@ -0,0 +1,48 @@
+let box = document.createElement("div");
+box.classList.add("box");
+box.classList.add("boxitem");
+box.style.width = "960px";
+box.style.height = "360px";
+box.style.backgroundColor = "gray";
+box.style.marginLeft = "200px";
+document.body.append(box);
+
+let box1 = document.createElement("div");
+box1.classList.add("box1");
+box1.classList.add("boxitem");
+box1.style.width = "290px";
+box1.style.height = "180px";
+box1.style.backgroundColor = "gray";
+box1.style.marginLeft = "200px";
+box1.style.marginTop = "40px";
+box1.style.display = "inline-block";
+document.body.append(box1);
+
+let box2 = document.createElement("div");
+box2.classList.add("box2");
+box2.classList.add("boxitem");
+box2.style.width = "290px";
+box2.style.height = "180px";
+box2.style.backgroundColor = "gray";
+box2.style.marginLeft = "45px";
+box2.style.marginTop = "40px";
+box2.style.display = "inline-block";
+document.body.append(box2);
+
+let box3 = document.createElement("div");
+box3.classList.add("box3");
+box3.classList.add("boxitem");
+box3.style.width = "290px";
+box3.style.height = "180px";
+box3.style.backgroundColor = "gray";
+box3.style.marginLeft = "45px";
+box3.style.marginTop = "40px";
+box3.style.display = "inline-block";
+document.body.append(box3); 
+
+
+
+
+
+
+
